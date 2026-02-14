46689387ef433fa36517aae1ace09e9db200e1c7
mohmedarfa451 <mohamedarfa451@gmail.com>
2026-02-14 02:01:51 +0200

    Initial commit - basic task manager

---
+++ script.js
@@ -0,0 +1,33 @@
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
+
+addBtn.addEventListener("click", addTask);
+
+function addTask() {
+    const taskText = taskInput.value.trim();
+
+    if (taskText === "") return;
+
+    const li = document.createElement("li");
+
+    const span = document.createElement("span");
+    span.textContent = taskText;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "X";
+
+    deleteBtn.addEventListener("click", function () {
+        li.remove();
+    });
+
+    span.addEventListener("click", function () {
+        li.classList.toggle("completed");
+    });
+
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+}
